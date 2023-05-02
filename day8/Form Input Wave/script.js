1ce8002ed0ddcae9bacd71fb0db8e3f5516642f3
Andre G <43078396+andregozubuyukoglu@users.noreply.github.com>
2023-05-02 17:36:30 +0300

    Delete day8/Form Input Wave directory

--- day8/Form Input Wave/script.js
+++
@@ -1,8 +0,0 @@
-const labels = document.querySelectorAll(".form-controllabel");
-
-labels.forEach((label) => {
-  label.innerHTML = label.innerText
-    .split("")
-    .map((letter, idx) => `<span>${letter}</span>`)
-    .join(" ");
-});
